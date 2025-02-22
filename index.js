ee02076a8acc07ef897b7dd18955b85d30abb530
Issa-WebDev <kissadev10@gmail.com>
2025-02-23 00:01:41 +0100

    mongoose

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { insertManyDoc } from "./models/movies.js";
+import { updateData } from "./models/movies.js";
 import connectDB from "./db/connectDB.js";
 const app = express();
 const port = process.env.PORT || 1000;
@@ -8,7 +8,7 @@ const DATABASE_URL =
 
 connectDB(DATABASE_URL);
 
-insertManyDoc();
+updateData("67b25f6fb3c7947630ad11b7");
 
 app.listen(port, () =>
   console.log(`Server is running on http://localhost:${port}`)
